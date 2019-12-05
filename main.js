9d3d4a678aa91841ad2393fc75059c0d412ad82c
antoinesolovyov <antoinesolovyov@gmail.com>
2019-12-05 09:49:22 +0300

    Add checkbox and delete handlers

--- main.js
+++ main.js
@@ -26,86 +26,78 @@ tableComponent.render();
 const keys = Object.keys(localStorage);
 
 for (let key of keys) {
-	const itemObject = JSON.parse(localStorage.getItem(key));
-	const itemComponent = new ItemComponent(itemObject);
+    const itemObject = JSON.parse(localStorage.getItem(key));
+    const itemComponent = new ItemComponent(itemObject);
 
-	tableComponent.list.items[itemComponent.id] = itemComponent;
-    tableComponent.list.count++;
+    tableComponent.list.items[itemComponent.id] = itemComponent;
+	tableComponent.list.count++;
+	
     if (!itemComponent.isFinished) tableComponent.list.unfinishedCount++;
     else tableComponent.list.finishedCount++;
 
-	tableComponent.bar.unfinishedTabText =
-		tableComponent.list.unfinishedCount;
-	tableComponent.bar.finishedTabText = tableComponent.list.finishedCount;
-	tableComponent.bar.allTabText = tableComponent.list.count;
+    tableComponent.bar.unfinishedTabText = tableComponent.list.unfinishedCount;
+    tableComponent.bar.finishedTabText = tableComponent.list.finishedCount;
+    tableComponent.bar.allTabText = tableComponent.list.count;
 
-	// checkbox click event
-	itemComponent.checkbox.addEventListener("click", () => {
-		if (itemComponent.isFinished) {
-			itemComponent.task.style.textDecoration = "none";
-			itemComponent.checkbox.style.backgroundImage =
-				"url('icons/square-icon.svg')";
-			itemComponent.isFinished = false;
-			tableComponent.list.unfinishedCount++;
-			tableComponent.list.finishedCount--;
-			tableComponent.bar.unfinishedTabText =
-				tableComponent.list.unfinishedCount;
-			tableComponent.bar.finishedTabText =
-				tableComponent.list.finishedCount;
-
-			itemObject.isFinished = false;
-			itemObject.textDecoration = "none";
-			itemObject.backgroundImage = "url('icons/square-icon.svg')";
-		} else {
-			itemComponent.task.style.textDecoration = "line-through";
-			itemComponent.checkbox.style.backgroundImage =
-				"url('icons/check-square-icon.svg')";
-			itemComponent.isFinished = true;
-			tableComponent.list.unfinishedCount--;
-			tableComponent.list.finishedCount++;
-			tableComponent.bar.unfinishedTabText =
-				tableComponent.list.unfinishedCount;
-			tableComponent.bar.finishedTabText =
-				tableComponent.list.finishedCount;
-			
-			itemObject.isFinished = true;
-			itemObject.textDecoration = "line-through";
-			itemObject.backgroundImage = "url('icons/check-square-icon.svg')";
-		}
-
-		localStorage.setItem(itemObject.id, JSON.stringify(itemObject));
-
-		tableComponent.render();
+    // checkbox click event
+    itemComponent.checkbox.addEventListener("click", () => {
+		checkboxHandler(itemObject, itemComponent)
 	});
 
-	// delete click event
-	itemComponent.delete.addEventListener("click", () => {
-		if (itemComponent.isFinished) {
-			tableComponent.list.finishedCount--;
-			tableComponent.bar.finishedTabText =
-				tableComponent.list.finishedCount;
-		} else {
-			tableComponent.list.unfinishedCount--;
-			tableComponent.bar.unfinishedTabText =
-				tableComponent.list.unfinishedCount;
-		}
+    // delete click event
+    itemComponent.delete.addEventListener("click", () => {
+        deleteHandler(itemObject, itemComponent)
+    });
 
-		localStorage.removeItem(itemObject.id);
-		delete tableComponent.list.items[itemComponent.id];
-		tableComponent.list.count--;
-		tableComponent.bar.allTabText = tableComponent.list.count;
-
-		tableComponent.render();
-	});
-
-	tableComponent.render();
+    tableComponent.render();
 }
+
+// input button onclick
+inputComponent.button.onclick = () => {
+    try {
+        if (inputComponent.input.value) {
+            const itemObject = new ItemObject(Date.now(), inputComponent.input.value, false
+            );
+            const itemComponent = new ItemComponent(itemObject);
+
+            inputComponent.input.value = "";
+
+            tableComponent.list.items[itemComponent.id] = itemComponent;
+			tableComponent.list.count++;
+			
+            if (!itemComponent.isFinished) tableComponent.list.unfinishedCount++;
+            else tableComponent.list.finishedCount++;
+
+            localStorage.setItem(itemObject.id, JSON.stringify(itemObject));
+
+            tableComponent.bar.unfinishedTabText = tableComponent.list.unfinishedCount;
+            tableComponent.bar.finishedTabText = tableComponent.list.finishedCount;
+            tableComponent.bar.allTabText = tableComponent.list.count;
+
+            // checkbox click event
+			itemComponent.checkbox.addEventListener("click", () => {
+				checkboxHandler(itemObject, itemComponent)
+			});
+
+            // delete click event
+            itemComponent.delete.addEventListener("click", () => {
+                deleteHandler(itemObject, itemComponent)
+            });
+
+            tableComponent.render();
+        }
+    } catch (e) {
+        console.log(e);
+    }
+
+    return false;
+};
 
 // unfinished tab click event
 tableComponent.bar.unfinished.addEventListener("click", () => {
-	tableComponent.bar.isUnfinished = true;
-	tableComponent.bar.isFinished = false;
-	tableComponent.bar.isAll = false;
+    tableComponent.bar.isUnfinished = true;
+    tableComponent.bar.isFinished = false;
+    tableComponent.bar.isAll = false;
 
     tableComponent.render();
 });
@@ -128,94 +120,54 @@ tableComponent.bar.all.addEventListener("click", () => {
     tableComponent.render();
 });
 
-// input button onclick
-inputComponent.button.onclick = () => {
+// checkbox handler
+function checkboxHandler(itemObject, itemComponent) {
+	if (itemComponent.isFinished) {
+		itemComponent.task.style.textDecoration = "none";
+		itemComponent.checkbox.style.backgroundImage = "url('icons/square-icon.svg')";
+		itemComponent.isFinished = false;
+		tableComponent.list.unfinishedCount++;
+		tableComponent.list.finishedCount--;
+		tableComponent.bar.unfinishedTabText = tableComponent.list.unfinishedCount;
+		tableComponent.bar.finishedTabText = tableComponent.list.finishedCount;
 
-	try {
+		itemObject.isFinished = false;
+		itemObject.textDecoration = "none";
+		itemObject.backgroundImage = "url('icons/square-icon.svg')";
+	} else {
+		itemComponent.task.style.textDecoration = "line-through";
+		itemComponent.checkbox.style.backgroundImage = "url('icons/check-square-icon.svg')";
+		itemComponent.isFinished = true;
+		tableComponent.list.unfinishedCount--;
+		tableComponent.list.finishedCount++;
+		tableComponent.bar.unfinishedTabText = tableComponent.list.unfinishedCount;
+		tableComponent.bar.finishedTabText = tableComponent.list.finishedCount;
 
-		if (inputComponent.input.value) {
-			const itemObject = new ItemObject(Date.now(), inputComponent.input.value, false);
-			const itemComponent = new ItemComponent(itemObject);
-
-			inputComponent.input.value = "";
-
-			tableComponent.list.items[itemComponent.id] = itemComponent;
-			tableComponent.list.count++;
-			if (!itemComponent.isFinished) tableComponent.list.unfinishedCount++;
-			else tableComponent.list.finishedCount++;
-	
-			localStorage.setItem(itemObject.id, JSON.stringify(itemObject));
-
-			tableComponent.bar.unfinishedTabText =
-				tableComponent.list.unfinishedCount;
-			tableComponent.bar.finishedTabText = tableComponent.list.finishedCount;
-			tableComponent.bar.allTabText = tableComponent.list.count;
-
-			// checkbox click event
-			itemComponent.checkbox.addEventListener("click", () => {
-				if (itemComponent.isFinished) {
-					itemComponent.task.style.textDecoration = "none";
-					itemComponent.checkbox.style.backgroundImage =
-						"url('icons/square-icon.svg')";
-					itemComponent.isFinished = false;
-					tableComponent.list.unfinishedCount++;
-					tableComponent.list.finishedCount--;
-					tableComponent.bar.unfinishedTabText =
-						tableComponent.list.unfinishedCount;
-					tableComponent.bar.finishedTabText =
-						tableComponent.list.finishedCount;
-
-					itemObject.isFinished = false;
-					itemObject.textDecoration = "none";
-					itemObject.backgroundImage = "url('icons/square-icon.svg')";
-				} else {
-					itemComponent.task.style.textDecoration = "line-through";
-					itemComponent.checkbox.style.backgroundImage =
-						"url('icons/check-square-icon.svg')";
-					itemComponent.isFinished = true;
-					tableComponent.list.unfinishedCount--;
-					tableComponent.list.finishedCount++;
-					tableComponent.bar.unfinishedTabText =
-						tableComponent.list.unfinishedCount;
-					tableComponent.bar.finishedTabText =
-						tableComponent.list.finishedCount;
-
-					itemObject.isFinished = true;
-					itemObject.textDecoration = "line-through";
-					itemObject.backgroundImage = "url('icons/check-square-icon.svg')";
-				}
-
-				localStorage.setItem(itemObject.id, JSON.stringify(itemObject));
-
-				tableComponent.render();
-			});
-
-			// delete click event
-			itemComponent.delete.addEventListener("click", () => {
-				if (itemComponent.isFinished) {
-					tableComponent.list.finishedCount--;
-					tableComponent.bar.finishedTabText =
-						tableComponent.list.finishedCount;
-				} else {
-					tableComponent.list.unfinishedCount--;
-					tableComponent.bar.unfinishedTabText =
-						tableComponent.list.unfinishedCount;
-				}
-
-				localStorage.removeItem(itemObject.id);
-				delete tableComponent.list.items[itemComponent.id];
-				tableComponent.list.count--;
-				tableComponent.bar.allTabText = tableComponent.list.count;
-
-				tableComponent.render();
-			});
-
-			tableComponent.render();
-		}
-		
-	} catch(e) {
-		console.log(e);
+		itemObject.isFinished = true;
+		itemObject.textDecoration = "line-through";
+		itemObject.backgroundImage =
+			"url('icons/check-square-icon.svg')";
 	}
 
-    return false;
-};
+	localStorage.setItem(itemObject.id, JSON.stringify(itemObject));
+
+    tableComponent.render();
+}
+
+// delete handler
+function deleteHandler(itemObject, itemComponent) {
+	if (itemComponent.isFinished) {
+		tableComponent.list.finishedCount--;
+		tableComponent.bar.finishedTabText = tableComponent.list.finishedCount;
+	} else {
+		tableComponent.list.unfinishedCount--;
+		tableComponent.bar.unfinishedTabText = tableComponent.list.unfinishedCount;
+	}
+
+	localStorage.removeItem(itemObject.id);
+	delete tableComponent.list.items[itemComponent.id];
+	tableComponent.list.count--;
+	tableComponent.bar.allTabText = tableComponent.list.count;
+
+	tableComponent.render();
+}
